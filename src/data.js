d5226b96330d63afcf13ce7ab3b658e87c39bc2e
darksh3ll <energie.sud83@gmail.com>
2019-07-27 13:01:00 +0200

    add data listCheckBox

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const listCheckBox = [
+    {id: 1, title: 'item1', isChecked: false},
+    {id: 2, title: 'item2', isChecked: false},
+    {id: 3, title: 'item3', isChecked: false},
+    {id: 4, title: 'item4', isChecked: false},
+  ];
