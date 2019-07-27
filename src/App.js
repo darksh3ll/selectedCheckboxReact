2773f7989797271cbb50edca6a6a102e3dafb17d
darksh3ll <energie.sud83@gmail.com>
2019-07-27 17:09:07 +0200

    creating application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { listCheckBox } from './data'
+import CheckBox from './components/Checkbox'
 
-function App() {
+export default () => {
+
+  const [selectedCheckbox, setSelectedCheckbox] = useState(listCheckBox);
+  const [allChecked, setAllChecked] = useState(false);
+  const checkAllItems = (isChecked) => {
+    setAllChecked(isChecked);
+    setSelectedCheckbox(selectedCheckbox.map((item) => ({ ...item, isChecked })))
+  };
+
+  const setItemChecked = (id, isChecked) => {
+    const items = selectedCheckbox.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          isChecked
+        }
+      }
+      return item;
+    });
+
+    setSelectedCheckbox(items);
+    setAllChecked(items.every(item => item.isChecked));
+  };
+
   return (
     <div className="App">
-
+      <h4 className="title">React-hooks-multi-select-checkbox</h4>
+      <CheckBox title={!allChecked ? "Check All" : "Uncheck All"} checked={allChecked} onChange={() => checkAllItems(!allChecked)} />
+      {selectedCheckbox.map(item => <CheckBox key={item.id} title={item.title} checked={item.isChecked} onChange={() => setItemChecked(item.id, !item.isChecked)} />)}
     </div>
   );
 }
-
-export default App;
